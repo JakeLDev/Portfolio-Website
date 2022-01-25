3076a34cc70a96773365cd4383c9bb0e49111e84
JakeL1001 <me@jakelyell.dev>
2022-01-25 18:53:18 +0800

    Split the pages

--- src/App.js
+++ src/App.js
@@ -3,14 +3,26 @@ import './App.css';
 import Navbar from './Components/Navbar';
 import MyFooter from './Components/MyFooter';
 import ScrollButton from './Components/ScrollButton';
+import ParticlesCanvas from './Components/Particles';
+import Banner from './Components/Banner';
 
-import Home from './Pages';
+import AboutMe from './Pages/About Me';
+import Projects from './Pages/Projects';
+import WorkExperience from './Pages/Work';
+import Events from './Pages/Events';
 
 function App() {
   return (
     <>
     <Navbar />
-    <Home/>
+    <Banner title="Software Engineer"/>
+    <ParticlesCanvas />
+
+    <AboutMe />
+    <Projects />
+    <WorkExperience />
+    {/* <Events /> */}
+
     <ScrollButton />
     <MyFooter />
     </>
---
+++ src/Pages/Events/index.js
@@ -0,0 +1,19 @@
+
+import React from 'react';
+
+const Events = () => {
+    return (
+        <>
+            <div className="page" id='Events'>
+                <h1 className="title">Events</h1>
+                <div className="paragraph">
+                    <p className="content" style={{textAlign: "center"}}>Here's some events that I've ran or been heavily involved in running.</p>
+                </div>
+                <hr className="Sub-separator"></hr>
+
+            </div>
+            <hr className="Section-separator"></hr>
+
+        </>)};
+
+export default Events;
