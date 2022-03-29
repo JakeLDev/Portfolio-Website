01632be54c9da40f6d4b09ac60fa9c41760c4acb
Jake Lyell <me@jakelyell.dev>
2022-03-29 14:53:35 +0800

    Update index.js

--- src/Components/Event/index.js
+++ src/Components/Event/index.js
@@ -7,14 +7,6 @@ import {
 const Event = (children) => {
     return (
         <>
-            {/* <div className="Event">
-                <h2 className="Event-header">{children.title}</h2>
-                <img className="Event-image" src={children.image} alt={children.title} />
-                <p className="Event-desc"> <b>Description</b>: {children.desc}</p>
-                <p className="Event-tools"> <b>Technologies</b>: {children.tools}</p>
-                <p className='Event-attendees'> <b>Attendees</b>: {children.attendees}</p>
-                <p className="Event-repo-link"> <b>Repository</b>: <a href={children.repolink} target="_blank" rel="noreferrer">{children.repolink}</a>{children.repo}</p>
-            </div> */}
             <div className="Event">
                 <h2 className="Event-header">{children.title}</h2>
                 {children.image.length > 0 &&
