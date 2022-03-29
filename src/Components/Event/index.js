bbb57c51a1ded4b9fb336c1cd8e0540fe59b3ffb
Jake Lyell <me@jakelyell.dev>
2022-03-29 14:53:14 +0800

    Added Trading Team Event
    and updated QFin logo

--- src/Components/Event/index.js
+++ src/Components/Event/index.js
@@ -1,14 +1,47 @@
 import React from 'react';
+import {
+    FaLinkedin,
+    FaFacebook,
+} from 'react-icons/fa';
 
 const Event = (children) => {
     return (
         <>
-            <div className="Event">
+            {/* <div className="Event">
                 <h2 className="Event-header">{children.title}</h2>
                 <img className="Event-image" src={children.image} alt={children.title} />
                 <p className="Event-desc"> <b>Description</b>: {children.desc}</p>
                 <p className="Event-tools"> <b>Technologies</b>: {children.tools}</p>
-                <p className="Event-repo-link"> <b>Repository</b>: <a href={children.repolink} target="_blank">{children.repolink}</a>{children.repo}</p>
+                <p className='Event-attendees'> <b>Attendees</b>: {children.attendees}</p>
+                <p className="Event-repo-link"> <b>Repository</b>: <a href={children.repolink} target="_blank" rel="noreferrer">{children.repolink}</a>{children.repo}</p>
+            </div> */}
+            <div className="Event">
+                <h2 className="Event-header">{children.title}</h2>
+                {children.image.length > 0 &&
+                    <img className="Event-image" src={children.image} alt={children.title} />
+                }
+                <p className="Event-date"><b>Date</b>: {children.date}</p>
+                <p className="Event-desc"> <b>Description</b>: {children.desc}</p>
+                {children.host.length > 0 &&
+                    <p className="Event-host"> <b>Host</b>: {children.host}</p>
+                }
+                {children.attendees.length > 0 &&
+                    <p className="Event-attendees"><b>Attendees</b>: {children.attendees}</p>
+                }
+                {children.facebook.length > 0 &&
+                    <a href={children.facebook}
+                        target="_blank"
+                        rel="noopener noreferrer">
+                        <FaFacebook className='contact-icon' />
+                    </a>
+                }
+                {children.linkedin.length > 0 &&
+                    <a href={children.linkedin}
+                        target="_blank"
+                        rel="noopener noreferrer">
+                        <FaLinkedin className='contact-icon' />
+                    </a>
+                }
             </div>
         </>
     );
--- src/Pages/About Me/index.js
+++ src/Pages/About Me/index.js
@@ -4,31 +4,31 @@ import Jake from '../../Components/assets/portraits/JakeLyell.jpg';
 import { Fade } from "react-awesome-reveal";
 
 const AboutMe = () => {
-    return (
-      <>
-        <div className="page" id='About Me'>
-          <div className="portrait-container">
-            <img className="portrait" src={Jake} alt="Portrait of Jake"></img>
-          </div>
-          <h1 className="title">About me</h1>
-          <div className="paragraph">
-            <p className="content" style={{textAlign: "center"}}>My name is Jake Lyell, I'm a 20 year old Computer Science student pursuing a career in Software Engineering.</p>
-          </div>
-          <h2 className="subheading">Education</h2>
-          <div className="paragraph">
-            <p className="content"> I'm currently studying a Bachelor of Science with a Double Major in Computer Science and Data Science at the University of Western Australia. Graduating in July 2022.
-                                    After graduation, I am planning to begin an Honours in Computer Science and Software Engineering in Semester 2, 2022. <br></br>
-                                    I graduated High School in 2018 with an ATAR of 94.25.
-                                    </p>
-          </div>
-          <h2 className="subheading">Hobbies</h2>
-          <div className="paragraph">
-            <p className="content"> I enjoy playing video games, building computers and completing coding projects either for my own amusement or for one of the university clubs that I am involved in.
-                                    I have also recently started building and programming keyboards, starting with the relatively simple GMMK Pro and moving onto soldering a Dumbpad (12 key macropad with rotary encoder).
-                                    Soon I will be building and programming a Sofle v2 split keyboard too.</p>
-          </div>
-          <h2 className="subheading">Skills</h2>
-          <Fade duration={2000}>
+  return (
+    <>
+      <div className="page" id='About Me'>
+        <div className="portrait-container">
+          <img className="portrait" src={Jake} alt="Portrait of Jake"></img>
+        </div>
+        <h1 className="title">About me</h1>
+        <div className="paragraph">
+          <p className="content" style={{ textAlign: "center" }}>My name is Jake Lyell, I'm a 20 year old Computer Science student pursuing a career in Software Engineering. Currently interviewing for Graduate Positions!</p>
+        </div>
+        <h2 className="subheading">Education</h2>
+        <div className="paragraph">
+          <p className="content"> I'm currently studying a Bachelor of Science with a Double Major in Computer Science and Data Science at the University of Western Australia. Graduating in July 2022.
+            After graduation, I am planning to begin an Honours in Computer Science and Software Engineering in Semester 2, 2022. <br></br>
+            I graduated High School in 2018 with an ATAR of 94.25.
+          </p>
+        </div>
+        <h2 className="subheading">Hobbies</h2>
+        <div className="paragraph">
+          <p className="content"> I enjoy playing video games, building computers and completing coding projects either for my own amusement or for one of the university clubs that I am involved in.
+            I have also recently started building and programming keyboards, starting with the relatively simple GMMK Pro and moving onto soldering a Dumbpad (12 key macropad with rotary encoder).
+            Soon I will be building and programming a Sofle v2 split keyboard too.</p>
+        </div>
+        <h2 className="subheading">Skills</h2>
+        <Fade duration={2000}>
           <ul className="skills">
             <li className="skill">
               <span class="iconify" data-icon="logos:python" data-width="60" data-height="60"></span>
@@ -39,57 +39,58 @@ const AboutMe = () => {
               <p>JavaScript</p>
             </li>
             <li className="skill">
-            <span class="iconify" data-icon="logos:git-icon" data-width="60" data-height="60"></span>
+              <span class="iconify" data-icon="logos:git-icon" data-width="60" data-height="60"></span>
               <p>Git</p>
             </li>
             <li className="skill">
-            <span class="iconify" data-icon="logos:html-5" data-width="60" data-height="60"></span>
+              <span class="iconify" data-icon="logos:html-5" data-width="60" data-height="60"></span>
               <p>HTML</p>
             </li>
             <li className="skill">
-            <span class="iconify" data-icon="logos:css-3" data-width="60" data-height="60"></span>
+              <span class="iconify" data-icon="logos:css-3" data-width="60" data-height="60"></span>
               <p>CSS</p>
             </li>
             <li className="skill">
-            <span class="iconify" data-icon="logos:react" data-width="60" data-height="60"></span>
+              <span class="iconify" data-icon="logos:react" data-width="60" data-height="60"></span>
               <p>React</p>
             </li>
             <li className="skill">
-            <span class="iconify" data-icon="logos:vue" data-width="60" data-height="60"></span>
+              <span class="iconify" data-icon="logos:vue" data-width="60" data-height="60"></span>
               <p>Vue.js</p>
             </li>
             <li className="skill">
-            <span class="iconify" data-icon="logos:r-lang" data-width="60" data-height="60"></span>
+              <span class="iconify" data-icon="logos:r-lang" data-width="60" data-height="60"></span>
               <p>R</p>
             </li>
             <li className="skill">
-            <span class="iconify" data-icon="logos:linux-tux" data-width="60" data-height="60"></span>
+              <span class="iconify" data-icon="logos:linux-tux" data-width="60" data-height="60"></span>
               <p>Linux</p>
             </li>
             <li className="skill">
-            <span class="iconify" data-icon="logos:mysql" data-width="60" data-height="60"></span>
+              <span class="iconify" data-icon="logos:mysql" data-width="60" data-height="60"></span>
               <p>MySQL</p>
             </li>
             <li className="skill">
-            <span class="iconify" data-icon="logos:java" data-width="60" data-height="60"></span>
+              <span class="iconify" data-icon="logos:java" data-width="60" data-height="60"></span>
               <p>Java</p>
             </li>
             <li className="skill">
-            <span class="iconify" data-icon="logos:c-plusplus" data-width="60" data-height="60"></span>
+              <span class="iconify" data-icon="logos:c-plusplus" data-width="60" data-height="60"></span>
               <p>C++</p>
             </li>
             <li className="skill">
-            <span class="iconify" data-icon="logos:c" data-width="60" data-height="60"></span>
+              <span class="iconify" data-icon="logos:c" data-width="60" data-height="60"></span>
               <p>C</p>
             </li>
             <li className="skill">
-            <span class="iconify" data-icon="logos:docker-icon" data-width="60" data-height="60"></span>
+              <span class="iconify" data-icon="logos:docker-icon" data-width="60" data-height="60"></span>
               <p>Docker</p>
             </li>
           </ul>
-          </Fade>
-        </div>
-        <hr className="Section-separator"></hr>
-    </>)};
+        </Fade>
+      </div>
+      <hr className="Section-separator"></hr>
+    </>)
+};
 
 export default AboutMe;
