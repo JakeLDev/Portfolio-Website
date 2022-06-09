e0582268ce4b91593db71e7ef0ce433600a014a7
JakeL1001 <me@jakelyell.dev>
2022-06-10 00:07:52 +0800

    Update Blog Posts

--- src/Pages/Blog/index.js
+++ src/Pages/Blog/index.js
@@ -3,6 +3,7 @@ import BlogPost from '../../Components/BlogPost';
 import Widget from '../../Components/Widget';
 
 import RESISTANCE from '../../Components/assets/projects/Resistance.png';
+import ATLASSIAN from '../../Components/assets/blog/Atlassian.png';
 
 const Blog = () => {
     return (
@@ -13,10 +14,10 @@ const Blog = () => {
                     <div className="blog-column">
                         <BlogPost
                             title="Accepted my first Software Engineering role"
-                            image={RESISTANCE}
-                            date="2020-01-01"
-                            desc1="testhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwww"
-                            desc2=""
+                            image={ATLASSIAN}
+                            date="25th of April 2022"
+                            desc1="Today I received a call from my recruiter at Atlassian, offering me a Graduate Software Engineering role to start in 2023. I was extremely happy, as I'd just missed out on Amazon at the final round of the hiring process."
+                            desc2="The start time of this role would allow me to complete my degree in June of 2022, and have some time off to work on my own projects and learn new technologies. Hopefully (covid-permitting) I can also spend some time travelling before starting a full-time role."
                             desc3="" />
                         </div>
                     <div className="widget-column">
@@ -26,6 +27,7 @@ const Blog = () => {
                                         <li>Back-end Web Development</li>
                                         <li>Java Web Frameworks</li>
                                         <li>Kotlin</li>
+                                        <li>AWS Deployment</li>
                                     </ul>
                             desc2=""
                             desc3="" />
