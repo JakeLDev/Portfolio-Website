e855cee943a079c23fb11d4baf361d308ab1a861
JakeLDev <me@jakelyell.dev>
2023-03-30 20:55:02 +0800

    Update Content Reduce size of images, remove unused images

--- src/Components/Job/Job.js
+++ src/Components/Job/Job.js
@@ -6,7 +6,7 @@ const Job = (children) => {
         <>
             <div id="Job" className='my-3 block h-auto max-w-95/100'>
                 {children.logo &&
-                    <img id="Job-logo" className="float-left inline-block object-cover pr-2" src={children.logo} alt={children.title} width="100px" height="100px"/>
+                    <img id="Job-logo" className="float-left inline-block pr-2" src={children.logo} alt={children.title} width="100px" height="100px"/>
                 }
                 {children.title &&
                     <h2 id="Job-title" className="mt-0 pb-1 font-semibold text-xl">{children.title}</h2>
@@ -17,8 +17,8 @@ const Job = (children) => {
                 {children.desc &&
                     <p id="Job-desc" className="py-2"> <b>Role: </b>{children.desc}</p>
                 }
-                <p id="Job-projects-title" className="py-2 text-xl"> <b>Projects:</b></p>
-                <div id='Job-projects' className="block pl-8">
+                <p id="Job-projects-title" className="py-2 text-xl max-lg:text-center"> <b>Projects:</b></p>
+                <div id='Job-projects' className="block lg:pl-8">
                     {children.projecttitle1 &&
                         <JobProject title={children.projecttitle1} desc={children.projectdesc1} image={children.projectimage1} repo={children.projectrepo1} repolink={children.projectrepolink1} tech={children.projecttech1}/>
                     }
--- src/Pages/Work/index.js
+++ src/Pages/Work/index.js
@@ -5,6 +5,8 @@ import Vinnies from '../../Components/assets/Gray+Design+Vinnies+logo.jpg';
 import Vintranet from '../../Components/assets/projects/Vintranet.png';
 import QFin from '../../Components/assets/QFin-Logo2.png';
 import QFinSite from '../../Components/assets/projects/QFin-site2.png';
+import Atlassian from '../../Components/assets/Atlassian.png';
+import Confluence from '../../Components/assets/projects/Confluence.png';
 import MeanReversionProject from '../../Components/assets/projects/MeanReversionProject.png';
 import CFC from '../../Components/assets/CFC.png';
 import WAIS from '../../Components/assets/projects/WAIS.png';
@@ -19,11 +21,22 @@ const WorkExperience = () => {
 
                 <div id="Jobs" className="block p-2">
 
-                    <Fade duration={1500} triggerOnce="true">
+                <Fade duration={1000} triggerOnce="true">
+                        <Job title="Atlassian | Junior Software Engineer" 
+                            logo={Atlassian} 
+                            daterange="January 2023 - Present" 
+                            desc="Joined Atlassian in January 2023 as a Graduate Software Engineer. Atlassian is a software company that develops products primarily for software teams. My role involves working on both frontend and backend development for the Atlassian's Confluence product. Currently working remotely from Perth, but looking to move to Sydney sometime soon."
+                                projecttitle1="Confluence Data Center"
+                                projectdesc1="Developing new features for Confluence Data Center, a product that allows users to collaborate on documents and share information. The product is used by both large and small companies worldwide. My team is responsible for developing new features for the product, as well as maintaining and fixing bugs in the existing codebase when they occur."
+                                projectimage1={Confluence}
+                                projecttech1="Java, React, JavaScript, TypeScript, Kotlin and more"/>
+                    </Fade>
+
+                    <Fade duration={1000} triggerOnce="true">
                         <Job title="QFin UWA | Executive Director of Trading" 
                             logo={QFin} 
                             daterange="November 2021 - August 2022" 
-                            desc="Recently elected Executive Director of Trading at QFin UWA for 2022. QFin is a club that focuses on trading stocks and cryptocurrencies, utilizing algorithmic approaches. The role involves creating, running and participating in projects for 2022’s Trading Team, along with organizing and running some trading-based events and workshops. Currently in the process of building both a website and an algorithm back testing framework for the club over summer. As a member of the Executive team, I am also involved in making decisions around the running of the club next year."
+                            desc="Worked as the Executive Director of Trading at QFin UWA for 2022. QFin is a club that focuses on trading stocks and cryptocurrencies, utilizing algorithmic approaches. The role involved creating, running and participating in projects for 2022’s Trading Team, along with organizing and running some trading-based events and workshops. I built both a website and an algorithm back-testing framework for the club over summer. As a member of the Executive team, I was also involved in making decisions around the running of the club in 2023."
                                 projecttitle1="QFin UWA Website | qfinuwa.com"
                                 projectdesc1="Created a Web Application and Email Domain for the QFin UWA club which displays what the club is about; our events, executive team and committee, and our sponsors. The site is accessible to both desktop and mobile users."
                                 projectimage1={QFinSite}
@@ -39,24 +52,23 @@ const WorkExperience = () => {
 
                     <hr className="Main-separator"></hr>
 
-                    <Fade duration={1500} triggerOnce="true">
+                    <Fade duration={1000} triggerOnce="true">
                         <Job logo={Vinnies}
                             title="St Vincent de Paul (Vinnies WA) | Web Developer" 
                             daterange="December 2021 - June 2022" 
-                            desc="Role: Web Developer creating the new intranet system for Vinnies WA called the 'Vintranet'" 
+                            desc="Worked as a Web Developer creating the new intranet system for Vinnies WA called the 'Vintranet'. This private site hosts all internal documents, templates and forms for Vinnies WA, and is used by hundreds of staff and volunteers. I worked with the IT team to develop the site, and populate it with all the necessary documents and forms." 
                                 projecttitle1='Vinnies WA "Vintranet"'
                                 projectdesc1="Developed and populated a new intranet system for Vinnies WA, for use across 52 stores, warehouse depos, and head office staff. This private site hosts all internal documents, templates and forms for Vinnies WA, and is used by hundreds of staff and volunteers."
                                 projectimage1={Vintranet}
                                 projecttech1="HTML, CSS, JavaScript, Custom CMS"/>
                     </Fade>
                     <hr className="Main-separator"></hr>
-
-                    {/* TODO Transfer this to Job type */}
-                    <Fade duration={1500} triggerOnce="true">
+                    
+                    <Fade duration={1000} triggerOnce="true">
                         <Job logo={CFC}
                             title="Coders for Causes UWA | Volunteer Software Engineer"
                             daterange="November 2021 - March 2022"
-                            desc="Volunteer developer for the Coders For Causes club at UWA. A club that provides volunteer software engineering teams for non-profit companies and charities. I participated in one of the summer projects for the Western Australian Institute of Sport by creating a web application for the track and field division."
+                            desc="Volunteer Developer for the Coders For Causes club at UWA. A club that provides volunteer software engineering teams for non-profit companies and charities. I participated in one of the summer projects for the Western Australian Institute of Sport by creating a web application for the track and field division."
                                 projecttitle1="Web Application for Western Australian Institute of Sport"
                                 projectdesc1="Developed a Web Application for WAIS to help visualise the performance of pole vaulters in training. We incorporated computer vision software and user input to allow coaches to best train their athletes. Worked in a large team of volunteers to develop the full stack of this application."
                                 projectimage1={WAIS}
